1a7e32312ecf6fdaa36ea0e2b98350b3bece1093
Kayla <6lackmacaroon@gmail.com>
2019-04-24 13:26:07 -0600

    create constructor function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import Todo from './components/TodoComponents/Todo';
+
 
 class App extends React.Component {
+  constructor () {
+    super () ;
+      this.state = {
+        todo: ''
+      
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
